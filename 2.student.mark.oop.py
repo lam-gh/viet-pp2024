2890080e906c87d8103298b00859c8c4ea23947d
lam-gh <190052983+lam-gh@users.noreply.github.com>
2024-12-23 13:31:59 +0700

    create Course class

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -7,6 +7,14 @@ class Student:
 
     def __str__(self):
         return f"ID: {self.id}, Name: {self.name}, DoB: {self.dob}"
+
+class Course:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __str__(self):
+        return f"ID: {self.id}, Name: {self.name}"
 # ================== MESSAGES ==================
 # ================== MAIN ==================
 def main():
