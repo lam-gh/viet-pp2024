0acda74e7cfa0d1858f25ef43ea18207086fac67
lam-gh <190052983+lam-gh@users.noreply.github.com>
2024-12-23 13:25:17 +0700

    create Student class

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,4 +1,12 @@
 # ================== CLASSES ==================
+class Student:
+    def __init__(self, id, name, dob):
+        self.id = id
+        self.name = name
+        self.dob = dob
+
+    def __str__(self):
+        return f"ID: {self.id}, Name: {self.name}, DoB: {self.dob}"
 # ================== MESSAGES ==================
 # ================== MAIN ==================
 def main():
